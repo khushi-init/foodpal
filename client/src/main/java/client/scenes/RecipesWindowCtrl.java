b0c1eba8a73bd1fd8d94138d6cfd3341096dd616
Dimitris Moulakis Koutsis <d.moulakiskoutsis@student.tudelft.nl>
2025-11-24 14:42:15 +0100

    Change the contents of the ObservableList from String to Recipe (Very useful)

--- client/src/main/java/client/scenes/RecipesWindowCtrl.java
+++ client/src/main/java/client/scenes/RecipesWindowCtrl.java
@@ -1,26 +1,31 @@
 package client.scenes;
 
+import client.RecipeListCell;
+import commons.Recipe;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
 
 public class RecipesWindowCtrl {
 
     @FXML
-    private ListView<String> sidebarRecipeNamesList;
+    private ListView<Recipe> sidebarRecipeNamesList;
 
     // This list will store the recipe names, they're automatically displayed in the sidebar
     // A selection listener should be implemented to handle clicks + deletes of recipes later
-    private final ObservableList<String> items = FXCollections.observableArrayList();
+    private final ObservableList<Recipe> recipes = FXCollections.observableArrayList();
 
     /**
      * Initializes the sidebar items (Recipe names) to track the ObervableList items
      */
     public void initialize() {
-        sidebarRecipeNamesList.setItems(items);
+        sidebarRecipeNamesList.setItems(recipes);
 
-        items.addAll("Recipe 1", "Recipe 2", "Recipe 3", "Recipe tree");
+        recipes.add(new Recipe("Eggs and bacon", null, null));
+
+        sidebarRecipeNamesList.setCellFactory(lc -> new RecipeListCell());
     }
 }
