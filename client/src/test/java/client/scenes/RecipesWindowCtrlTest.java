b98df3ff5d120045ccf211a13d7c3ddb0ac193da
Adel Bežová <a.bezova@student.tudelft.nl>
2025-11-28 19:29:38 +0100

    fixing cloning recipe + testing naming method

---
+++ client/src/test/java/client/scenes/RecipesWindowCtrlTest.java
@@ -0,0 +1,78 @@
+package client.scenes;
+
+import client.utils.ErrorCtrl;
+import commons.Recipe;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RecipesWindowCtrlTest {
+    private RecipesWindowCtrl window;
+    private PrimaryCtrl primary;
+    private ErrorCtrl error;
+
+    @BeforeEach
+    public void setup() {
+        window = new RecipesWindowCtrl(primary, error);
+    }
+
+    private String invokeCreateCopyName(String baseName) throws Exception {
+        Method m = RecipesWindowCtrl.class.getDeclaredMethod("createCopyName", String.class);
+        m.setAccessible(true);
+        return (String) m.invoke(window, baseName);
+    }
+
+    private void setRecipes(ObservableList<Recipe> recipes) throws Exception {
+        Field field = RecipesWindowCtrl.class.getDeclaredField("recipes");
+        field.setAccessible(true);
+        field.set(window, recipes);
+    }
+
+
+
+    @Test
+    public void createCopyNameTestCreatesBaseNameWithOneWhenNoCopies() throws Exception {
+        ObservableList<Recipe> recipes = FXCollections.observableArrayList(
+                new Recipe("Soup", null, List.of()),
+                new Recipe("Pasta", null, List.of()),
+                new Recipe("Salad", null, List.of())
+        );
+        setRecipes(recipes);
+        String copyName = invokeCreateCopyName("Salad");
+        assertEquals("Salad(1)", copyName);
+
+    }
+
+    @Test
+    public void createCopyNameTestCreatesBaseNameWithTwoWhenOneCopy() throws Exception {
+        ObservableList<Recipe> recipes = FXCollections.observableArrayList(
+                new Recipe("Soup", null, List.of()),
+                new Recipe("Pasta", null, List.of()),
+                new Recipe("Salad", null, List.of()),
+                new Recipe("Salad(1)", null, List.of())
+        );
+        setRecipes(recipes);
+        String copyName = invokeCreateCopyName("Salad");
+        assertEquals("Salad(2)", copyName);
+    }
+
+    @Test
+    public void createCopyNameTestCreatesBaseNameWithOneOneWhenCopyingACopy() throws Exception {
+        ObservableList<Recipe> recipes = FXCollections.observableArrayList(
+                new Recipe("Soup", null, List.of()),
+                new Recipe("Pasta", null, List.of()),
+                new Recipe("Salad", null, List.of())
+        );
+        setRecipes(recipes);
+        String copyName = invokeCreateCopyName("Salad");
+        String copyName2 = invokeCreateCopyName(copyName);
+        assertEquals("Salad(1)(1)", copyName2);
+    }
+}
