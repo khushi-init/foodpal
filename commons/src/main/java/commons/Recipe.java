850f68a4ff4fc21a54b07cd171783baaf2f80cd7
Khushi Agrawal <k.a.agrawal@student.tudelft.nl>
2025-11-29 14:42:24 +0100

    Created a composite key for RecipeIngredient

--- commons/src/main/java/commons/Recipe.java
+++ commons/src/main/java/commons/Recipe.java
@@ -16,6 +16,7 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     // One Recipe has MANY RecipeIngredients (the join entity)
