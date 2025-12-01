22629e987155ca2f883d5249e631fe891b14b6c8
Luca Weesie <L.Weesie@student.tudelft.nl>
2025-12-01 21:06:42 +0100

    Added an UI for the shopping list along with the Shopping List Class.

--- client/src/main/java/client/scenes/ShoppingListCtrl.java
+++ client/src/main/java/client/scenes/ShoppingListCtrl.java
@@ -1,4 +1,100 @@
 package client.scenes;
 
-public class ShoppingListControl {
+import client.Main;
+import client.utils.ShoppingListIngredientUICtrl;
+import commons.ShoppingList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.util.Pair;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ShoppingListCtrl {
+
+    @FXML
+    private VBox shoppingListView;
+
+    private ShoppingList shoppingList;
+
+    /**
+     * Initialize the scene.
+     */
+    public void initialize() {
+        shoppingList = new ShoppingList();
+        shoppingList.addIngredient("TEST");
+        showShoppingList();
+    }
+
+    /**
+     * Show the shopping list.
+     */
+    public void showShoppingList() {
+        shoppingListView.getChildren().clear();
+        List<String> shoppingListIngredients = shoppingList.getIngredients();
+
+        for (int i = 0; i < shoppingListIngredients.size(); i++) {
+            Pair<ShoppingListIngredientUICtrl, Node> pair =
+                    Main.FXML.loadNode(
+                            ShoppingListIngredientUICtrl.class,
+                            "client", "modules", "ShoppingListIngredient.fxml"
+                    );
+
+            ShoppingListIngredientUICtrl ctrl = pair.getKey();
+            Node node = pair.getValue();
+
+            ctrl.setText("- " + shoppingListIngredients.get(i));
+            ctrl.setIndex(i);
+
+            VBox.setVgrow(node, Priority.NEVER);
+            shoppingListView.getChildren().add(node);
+
+            ctrl.setDeleteCheck(() -> {
+                shoppingListView.getChildren().remove(node);
+            });
+            shoppingListView.requestLayout();
+        }
+
+        Button addButton = new Button("Add Ingredient");
+        shoppingListView.getChildren().add(addButton);
+        addButton.setOnAction(e -> {
+            TextInputDialog ingredientNameDialog = new TextInputDialog();
+            ingredientNameDialog.setTitle("Add ingredient to shopping list");
+            ingredientNameDialog.setHeaderText("Add a new ingredient");
+            ingredientNameDialog.setContentText("Ingredient name:");
+
+            Optional<String> result = ingredientNameDialog.showAndWait();
+            result.ifPresent(name -> {
+                if(!name.trim().isEmpty()){
+                    Optional<String> amount = getAmount(name);
+
+                    amount.ifPresentOrElse(x -> {
+                        if (!x.isEmpty()) {
+                            shoppingList.addIngredient(name.trim() + " (" + x + ")");
+                        } else {
+                            shoppingList.addIngredient(name.trim());
+                        }
+                    }, () -> {
+                        shoppingList.addIngredient(name.trim());
+                    });
+
+                    showShoppingList();
+                }
+            });
+        });
+    }
+
+    private Optional<String> getAmount(String name) {
+        TextInputDialog ingredientAmountDialog = new TextInputDialog();
+        ingredientAmountDialog.setTitle("Add ingredient to shopping list");
+        ingredientAmountDialog.setTitle("Add a new ingredient");
+        ingredientAmountDialog.setContentText("Amount (e.g. 50g):");
+        Optional<String> amount = ingredientAmountDialog.showAndWait();
+
+        return amount;
+    }
 }
--- client/src/main/java/client/utils/ShoppingListIngredientUICtrl.java
+++ client/src/main/java/client/utils/ShoppingListIngredientUICtrl.java
@@ -1,4 +1,65 @@
 package client.utils;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+import java.util.function.Consumer;
+
 public class ShoppingListIngredientUICtrl {
+
+    @FXML
+    private Label ingredientText;
+
+    @FXML
+    private HBox ingredientBox;
+
+    private Runnable deleteCheck;
+
+    private Consumer<String> editInstruction;
+
+    private long index;
+
+    public void setText(String text){
+        ingredientText.setText(text);
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public long getIndex() {
+        return this.index;
+    }
+
+    public void setDeleteCheck(Runnable deleteCheck){
+        this.deleteCheck = deleteCheck;
+    }
+
+    public void setEditInstruction(Consumer<String> editInstruction) {
+        this.editInstruction = editInstruction;
+    }
+
+    @FXML
+    private void handleDeleteButton(){
+        if(deleteCheck != null){
+            deleteCheck.run();
+        }
+    }
+
+    @FXML
+    public void handleEditButton() {
+        TextField textField = new TextField(ingredientText.getText().replace("- ", ""));
+        HBox.setHgrow(textField, Priority.ALWAYS);
+        textField.setPromptText(ingredientText.getText());
+        textField.setFocusTraversable(false);
+        textField.setOnAction(actionEvent -> {
+            editInstruction.accept(textField.getText());
+            ingredientBox.getChildren().set(0, ingredientText);
+        });
+        ingredientBox.getChildren().set(0, textField);
+        textField.requestFocus();
+    }
 }
