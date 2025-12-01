0a22716f33c51df2605938e4bc45583486cdfd14
Luca Weesie <L.Weesie@student.tudelft.nl>
2025-12-01 21:10:14 +0100

    Removed the edit button functionality taken of another controller,

--- client/src/main/java/client/utils/ShoppingListIngredientUICtrl.java
+++ client/src/main/java/client/utils/ShoppingListIngredientUICtrl.java
@@ -2,10 +2,7 @@ package client.utils;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-
 import java.util.function.Consumer;
 
 public class ShoppingListIngredientUICtrl {
@@ -51,15 +48,6 @@ public class ShoppingListIngredientUICtrl {
 
     @FXML
     public void handleEditButton() {
-        TextField textField = new TextField(ingredientText.getText().replace("- ", ""));
-        HBox.setHgrow(textField, Priority.ALWAYS);
-        textField.setPromptText(ingredientText.getText());
-        textField.setFocusTraversable(false);
-        textField.setOnAction(actionEvent -> {
-            editInstruction.accept(textField.getText());
-            ingredientBox.getChildren().set(0, ingredientText);
-        });
-        ingredientBox.getChildren().set(0, textField);
-        textField.requestFocus();
+        // TODO
     }
 }
