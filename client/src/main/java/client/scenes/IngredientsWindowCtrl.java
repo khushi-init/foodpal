451bad88bfc899d40d8bdad03f0de508835743a1
Khushi Agrawal <k.a.agrawal@student.tudelft.nl>
2025-12-02 17:13:46 +0100

    Added basic initialization methods for the IngredientWindow

--- client/src/main/java/client/scenes/IngredientsWindowCtrl.java
+++ client/src/main/java/client/scenes/IngredientsWindowCtrl.java
@@ -1,9 +1,13 @@
 package client.scenes;
 
+import client.IngredientListCell;
 import client.Main;
 import client.utils.ErrorCtrl;
 import client.utils.ServerUtils;
 import commons.Ingredient;
+import jakarta.inject.Inject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -32,6 +36,34 @@ public class IngredientsWindowCtrl {
     private Button remove;
     @FXML
     private Button refresh;
-    @FXML
-    private Button backButton;
+
+    private ObservableList<Ingredient> ingredients;
+
+    /**
+     * Injectable constructor is REQUIRED for Guice to provide dependencies.
+     * @param p PrimaryCtrl instance for scene switching.
+     * @param c ErrorCtrl instance for displaying errors.
+     */
+    @Inject
+    public IngredientsWindowCtrl(PrimaryCtrl p, ErrorCtrl c) {
+        this.primaryCtrl = p;
+        this.errorCtrl = c;
+    }
+
+    public void initialize(){
+        ingredients = FXCollections.observableArrayList(server.getIngredients());
+        sidebarIngredientNamesList.setItems(ingredients);
+        sidebarIngredientNamesList.setCellFactory(icl -> new IngredientListCell());
+        sidebarIngredientNamesList.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue,
+                              newValue) -> {
+                    if(newValue != null){
+                        openIngredient(newValue);
+                    }
+                });
+    }
+
+    public void openIngredient(Ingredient ingredient){
+
+    }
 }
