0ca8d503346c23f2acaaa46b7260ad4d3e3b128c
Marnix van Velzen <m.b.velzen@student.tudelft.nl>
2025-12-04 21:56:59 +0100

    Add minimal advanced search, to be updated later

--- client/src/main/java/client/utils/SearchCtrl.java
+++ client/src/main/java/client/utils/SearchCtrl.java
@@ -10,38 +10,51 @@ import commons.Ingredient;
 public class SearchCtrl {
     /**
      * Performs a query on the provided List of recipes
-     * @param query For now a simple String the recipe has to contain
+     * @param query A String containing words recipes must contain, separated by spaces (case insensitive)
      * @param recipes A list of all recipes in which to search
-     * @return A List of all recipes that satisfy the search condition
+     * @return A List of all recipes that satisfy the search conditions
      */
     public List<Recipe> search(String query, List<Recipe> recipes){
         query = query.toLowerCase();
+        List<String> queries = List.of(query.split(" "));
         ArrayList<Recipe> filteredRecipes = new ArrayList<>();
 
         for(Recipe recipe : recipes){
-            List<RecipeIngredient> recipeIngredients = recipe.getIngredients();
-            List<Ingredient> ingredients = recipeIngredients.stream()
-                    .map(x -> x.getIngredient()).toList();
-            for(Ingredient ingredient : ingredients){
-                if(ingredient.getName().toLowerCase().contains(query)){
-                    filteredRecipes.add(recipe);
-                    continue;
-                }
-            }
-
-            List<String> instructions = recipe.getPreparationSteps();
-            for(String instruction: instructions){
-                if(instruction.toLowerCase().contains(query)){
-                    filteredRecipes.add(recipe);
-                    continue;
-                }
-            }
-
-            if(recipe.getName().toLowerCase().contains(query)){
+            //check if this recipe satisfies all queries
+            if(queries.stream().allMatch(x -> checkIfRecipeSatisfiesQuery(x, recipe))){
                 filteredRecipes.add(recipe);
-                continue;
             }
         }
         return filteredRecipes;
+    }
+
+    /**
+     * Checks if a recipe satisfies a single query
+     * @param query String with single query (is not parsed for spaces), not case sensitive
+     * @param recipe The recipe to check
+     * @return boolean indicating if the recipe satisfies the String query
+     */
+    public boolean checkIfRecipeSatisfiesQuery(String query, Recipe recipe){
+        List<RecipeIngredient> recipeIngredients = recipe.getIngredients();
+        List<Ingredient> ingredients = recipeIngredients.stream()
+                .map(x -> x.getIngredient()).toList();
+        for(Ingredient ingredient : ingredients){
+            if(ingredient.getName().toLowerCase().contains(query)){
+                return true;
+            }
+        }
+
+        List<String> instructions = recipe.getPreparationSteps();
+        for(String instruction: instructions){
+            if(instruction.toLowerCase().contains(query)){
+                return true;
+            }
+        }
+
+        if(recipe.getName().toLowerCase().contains(query)){
+            return true;
+        }
+
+        return false;
     }
 }
