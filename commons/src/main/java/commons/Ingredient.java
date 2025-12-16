ab658281fe24267ee65b78f60a75f8edbdfb5b20
Khushi Agrawal <k.a.agrawal@student.tudelft.nl>
2025-12-16 20:33:55 +0100

    Trying to fix error when opening recipe app

--- commons/src/main/java/commons/Ingredient.java
+++ commons/src/main/java/commons/Ingredient.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.Set;
 
