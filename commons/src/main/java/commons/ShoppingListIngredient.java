e4fd3894ed6435e872505766f03c7a18fa7569b4
Luca Weesie <L.Weesie@student.tudelft.nl>
2025-12-18 10:33:24 +0100

    Equals/hashcode method for ShoppingListIngredient & changed testToMarkdown to fit date

--- commons/src/main/java/commons/ShoppingListIngredient.java
+++ commons/src/main/java/commons/ShoppingListIngredient.java
@@ -1,10 +1,16 @@
 package commons;
 
+import java.util.Objects;
+
 public class ShoppingListIngredient {
 
     private String nameAmount;
     private boolean checkedOff;
 
+    /**
+     * Create a new ShoppingListIngredient
+     * @param nameAmount - A string with the name and amount
+     */
     public ShoppingListIngredient(String nameAmount) {
         this.nameAmount = nameAmount;
         this.checkedOff = false;
@@ -24,5 +30,17 @@ public class ShoppingListIngredient {
 
     public void setCheckedOff(boolean checkedOff) {
         this.checkedOff = checkedOff;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoppingListIngredient that = (ShoppingListIngredient) o;
+        return checkedOff == that.checkedOff && Objects.equals(nameAmount, that.nameAmount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAmount, checkedOff);
     }
 }
