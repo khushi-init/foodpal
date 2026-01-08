2f22bbcbbaa8fd2673802e4ea589158f1783bb4f
Dimitris Moulakis Koutsis <d.moulakiskoutsis@student.tudelft.nl>
2026-01-08 15:21:24 +0100

    OUR CHECKSTYLE SHOULDNT APPLY TO TESTS

--- server/src/test/java/server/service/RecipeServiceTest.java
+++ server/src/test/java/server/service/RecipeServiceTest.java
@@ -2,7 +2,6 @@ package server.service;
 
 import commons.Recipe;
 import commons.TitleUpdate;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
