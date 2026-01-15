ff86363db31c75913a36972d957f3b8da78333de
Loet Wagener <l.j.wagener@student.tudelft.nl>
2026-01-15 17:56:45 +0100

    show favorite deletion warning upon refresh

--- client/src/main/java/client/scenes/RecipesWindowCtrl.java
+++ client/src/main/java/client/scenes/RecipesWindowCtrl.java
@@ -1201,6 +1201,8 @@ public class RecipesWindowCtrl {
             if (!recipeStillExists) return;
             setSelectedRecipe(selectedRecipe);
 
+            dataManipulator.loadFavs();
+
         } catch (Exception e) {
             errorCtrl.showGenericError(e);
         }
