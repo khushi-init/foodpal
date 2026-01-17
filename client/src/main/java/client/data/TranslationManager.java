bbed68d8c8b714744ee1b83a402bccf347401d6b
Adel Bežová <a.bezova@student.tudelft.nl>
2026-01-17 20:59:34 +0100

    documentation for translation manager

--- client/src/main/java/client/data/TranslationManager.java
+++ client/src/main/java/client/data/TranslationManager.java
@@ -11,15 +11,30 @@ public class TranslationManager {
     private final ObjectProperty<ResourceBundle> bundle = new SimpleObjectProperty<>();
     private Locale currentLocale;
 
+    /**
+     *
+     *Constructor for TranslationManager, initializes with default language English
+     */
     public TranslationManager() {
         setLanguage(Locale.ENGLISH);
     }
 
+    /**
+     *Sets the active language for the UI and loads the corresponding resource bundle.
+     *@param locale the locale to switch the application language to
+     */
     public void setLanguage(Locale locale) {
         this.currentLocale = locale;
         this.bundle.set(ResourceBundle.getBundle("i18n.messages", locale));
     }
 
+    /**
+     *Translates the given key using the active language.
+     *If the key is not found, the key itself is returned.
+     *@param key the translation key
+     *@param args optional arguments for formatted messages
+     *@return the translated string or the key if no translation is found
+     */
     public String tr(String key, Object... args) {
         if (bundle.get() == null) return key;
         try {
@@ -30,10 +45,18 @@ public class TranslationManager {
         }
     }
 
+    /**
+     *Returns the resource bundle property used for translations. --> where translates text lives
+     *@return the current resource bundle property
+     */
     public ObjectProperty<ResourceBundle> bundleProperty() {
         return bundle;
     }
 
+    /**
+     *Returns the currently active locale. -->which language is currently set
+     *@return the current locale
+     */
     public Locale getCurrentLocale() {
         return currentLocale;
     }
